c02e44dbf0bd6fb0f88ea26014a1481b8458dd8d
rijha16cs <rijh16cs@cmrit.ac.in>
2017-03-07 14:31:16 +0530

    find roots of quadratic equation

---
+++ week1/quaraticroots.c
@@ -0,0 +1,26 @@
+#include <math.h>
+#include <stdio.h>
+int main()
+{
+float a,b,c,discriminant,root1,root2;
+printf("Input values of a,b and c.");
+scanf("%f%f%f",&a,&b,&c);
+discriminant = b*b-4*a*c;
+if(discriminant<0)
+{
+printf("\nRoots are imaginary");
+}
+else if(discriminant==0)
+{
+printf("\nRoots are equal");
+root1 = -b/(2.0*a);
+printf("Root of quadratic equation is %.2f\n" ,root1);
+} 
+else
+{
+root1 = (-b + sqrt(discriminant))/(2.0*a);
+root2 = (-b - sqrt(discriminant))/(2.0*a);
+printf("\nRoot1 = %.2f\nRoot2 = %.2f\n", root1,root2);
+}
+return 0;
+}
